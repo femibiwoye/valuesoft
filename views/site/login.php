8c51aec99a0524b2467bcf36039569227ab05ed2
femiparadise <femibiwoye@gmail.com>
2018-08-03 19:57:35 +0100

    UI and authentication implementation

---
+++ views/site/login.php
@@ -0,0 +1,35 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Url;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="page-content--bge5">
+    <div class="container">
+        <div class="login-wrap">
+            <div class="login-content">
+                <div class="login-logo">
+                    <a href="<?=Yii::$app->homeUrl?>">
+                        <img src="<?=Url::to('@web/')?>images/logo.png" alt="CoolAdmin">
+                    </a>
+                </div>
+                <div class="login-form">
+                    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                    <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+
+                    <?= $form->field($model, 'password')->passwordInput() ?>
+
+                        <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign in</button>
+                    <?php ActiveForm::end(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
